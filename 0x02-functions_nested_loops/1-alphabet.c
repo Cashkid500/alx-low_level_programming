ee4ecee27ceacf02d6c724515b5ae6a1664c41ed
Balogun Murtadho <murtadhob@gmail.com>
2022-03-15 15:21:45 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
+ */
+
+void print_alphabet(void)
+{
+	int lw = 'a';
+
+	while (lw <= 'z')
+	{
+		_putchar(lw);
+		lw += 1;
+	}
+	_putchar(10);
+}
