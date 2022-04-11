0ed988c3ead091ffd0c5a2fbbd1b399d1e261333
Balogun Murtadho <murtadhob@gmail.com>
2022-04-11 16:53:30 +0100

    A function-like macro ABS(x) that computes the absolute value of a number x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,13 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: Balogun Murtadho
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
